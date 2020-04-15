ab2f7eda6c7e82f7ede4cd13d0370aa83bd5351e
wez3 <wez3@users.noreply.github.com>
2020-04-15 16:52:13 +0200

    Update application checks

--- Azure/AzureAD/checks/checkAddApplicationEnabled.ps1
+++ Azure/AzureAD/checks/checkAddApplicationEnabled.ps1
@@ -1,1 +1,7 @@
-Get-MsolCompanyInformation | select UsersPermissionToCreateLOBAppsEnabled
+$enabled = Get-MsolCompanyInformation | select UsersPermissionToCreateLOBAppsEnabled
+
+if ($enabled) {
+    Write-Host "TRUE: Users are allowed to register applications."
+} else {
+    Write-Host "FALSE: Users are NOT allowed to register applications."
+}
--- Azure/AzureAD/checks/checkApplicationConsentsEnabled.ps1
+++ Azure/AzureAD/checks/checkApplicationConsentsEnabled.ps1
@@ -1,1 +1,7 @@
-Get-MsolCompanyInformation | select UsersPermissionToUserConsentToAppEnabled
+$enabled = Get-MsolCompanyInformation | select UsersPermissionToUserConsentToAppEnabled
+
+if ($enabled) {
+    Write-Host "TRUE: Users are allowed to consent to applications."
+} else {
+    Write-Host "FALSE: Users are NOT allowed to consent to applications."
+}
